a851fe440a38cdbb1e5d712c9bea28567f68e758
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2025-12-05 21:28:38 -0600

    Write cli app boilerplate

--- 25/app/Main.hs
+++ 25/app/Main.hs
@@ -1,4 +1,28 @@
 module Main where
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode (..))
+import System.IO
+import Text.Read (readMaybe)
+
+argsToDay :: [String] -> Maybe Int
+argsToDay [x] = readMaybe x
+argsToDay _ = Nothing
+
+panicWithMessage :: String -> IO ()
+panicWithMessage message = do
+    hPutStrLn stderr message
+    exitWith (ExitFailure 1)
+
+solutions :: [(Int, String -> String)]
+solutions =
+    [
+    ]
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+    case argsToDay args of
+        Nothing -> panicWithMessage "Supply the day (1-25) as an argument"
+        Just day -> case lookup day solutions of
+            Nothing -> panicWithMessage $ "No such day " ++ show day
+            Just solution -> interact solution
