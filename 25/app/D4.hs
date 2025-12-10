30ea908d156051ab22b18485173e78f5a31baaa6
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2025-12-09 20:25:06 -0600

    Day 4

---
+++ 25/app/D4.hs
@@ -0,0 +1,55 @@
+module D4 where
+
+-- succumb to indexing instead of rotation
+surroundingCoords :: Int -> Int -> (Int, Int) -> [(Int, Int)]
+surroundingCoords n m (i, j) = [
+        (i', j') |
+        i' <- [(i-1)..(i+1)],
+        j' <-[(j-1)..(j+1)],
+        i' >= 0 && j' >= 0 && i' < n && j' < m && (i, j) /= (i', j')
+    ]
+
+index2d :: [[a]] -> (Int, Int) -> a
+index2d matrix (i, j) = matrix !! i !! j
+
+outerProduct :: (a -> b -> c) -> [a] -> [b] -> [[c]]
+outerProduct f as bs = [[f a b | b <- bs] | a <- as]
+
+mask :: String -> [[Int]]
+mask = map (map (fromEnum . (==)'@')) . lines
+
+d4p1 :: String -> Int
+d4p1 input =
+    length .
+    filter (<4) .
+    map (sum . map (index2d bitmask) . surroundingCoords n m) .
+    filter ((==1) .  index2d bitmask) .
+    concat
+    $ coordMatrix
+    where
+        bitmask = mask input
+        n = length bitmask
+        m = length $ head bitmask
+        iota x = [0..(x-1)]
+        coordMatrix = outerProduct (,) (iota n) (iota m)
+
+
+d4p2 :: String -> Int
+d4p2 input = length setCoords - length remaining
+    where
+        bitmask = mask input
+        n = length bitmask
+        m = length $ head bitmask
+        iota x = [0..(x-1)]
+        coordMatrix = outerProduct (,) (iota n) (iota m)
+        setCoords = filter ((==1) .  index2d bitmask) .  concat $ coordMatrix
+        remaining = until (\x -> step x == x) step setCoords
+        step :: [(Int, Int)] -> [(Int, Int)]
+        step cs =
+            map fst .
+            filter ((>=4) . snd) .
+            map (\c -> (c, length . filter (`elem`cs) . surroundingCoords n m $ c))
+            $ cs
+
+d4 :: String -> String
+d4 input = unlines . map (show . ($ input)) $ [d4p1, d4p2]
